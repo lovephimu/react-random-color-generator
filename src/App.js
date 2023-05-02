e08a7a79c524a8cc1bd3c861c7ed0a2b5df66e32
lovephimu <soe.nlb@gmail.com>
2023-05-02 20:40:09 +0200

    add color transition by css

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ export default function App() {
       <div className="wrapper">
         <div className="colorCard">
           <div
-            className="colorGeneratorBox"
+            className="colorGeneratorBox colorGeneratorBoxTransition"
             style={{ backgroundColor: newColor }}
           >
             Generated Color:
