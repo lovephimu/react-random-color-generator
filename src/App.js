8cf152c5b87b86f8db2f7f90b1f648b3c5a63d0f
lovephimu <soe.nlb@gmail.com>
2023-05-02 18:56:18 +0200

    add styling and color generation

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
-import React from 'react';
+import randomColor from 'randomcolor'; // import the script
+import React, { useState } from 'react';
 
 export default function App() {
+  const [newColor, setNewColor] = useState(randomColor());
+
   return (
     <>
       <h1 className="title">Random Color Generator</h1>
       <div className="wrapper">
-        <div className="colorGeneratorBox">This is gonna be a box</div>
+        <div className="colorCard">
+          <div
+            className="colorGeneratorBox"
+            style={{ backgroundColor: newColor }}
+          >
+            Generated Color:
+            <p className="newColor">{newColor.toUpperCase()}</p>
+          </div>
+          <button className="button" onClick={() => setNewColor(randomColor())}>
+            Generate
+          </button>
+          <div className="description">
+            Push the button to generate a random color
+          </div>
+        </div>
       </div>
     </>
   );
